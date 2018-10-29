0b482e75aec4c43d14ace6e49acc74d46dd30c37
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2018-10-29 10:54:28 +0100

    Add sidedrawer with backdrop funcitonality

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const Layout = props => {
-  return (
-    <>
-      <Toolbar></Toolbar>
-      <main className={classes.Content}>{props.children}</main>
-    </>
-  );
-};
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false})
+  }
+
+  render() {
+    return (
+      <>
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+        <Toolbar />
+        <main className={classes.Content}>{this.props.children}</main>
+      </>
+    );
+  }
+}
 
 export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,17 @@
 import React from "react";
 import classes from "./Toolbar.module.css";
-import Logo from './../../Logo/Logo';
+import Logo from "./../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 
 const Toolbar = () => (
   <header className={classes.Toolbar}>
     <div>Menu</div>
-    <Logo></Logo>
-    <NavigationItems></NavigationItems>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
   </header>
 );
 

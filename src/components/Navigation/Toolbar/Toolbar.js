55b17d0a86217c26e33049695d931b9e943ed475
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2018-10-29 09:44:19 +0100

    Add navigation

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import classes from "./Toolbar.module.css";
 import Logo from './../../Logo/Logo';
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const Toolbar = () => (
   <header className={classes.Toolbar}>
     <div>Menu</div>
     <Logo></Logo>
-    <nav>...</nav>
+    <NavigationItems></NavigationItems>
   </header>
 );
 

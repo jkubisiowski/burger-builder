5de0590b7bb71b81260ccf7beebc65e2003fded9
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2018-10-30 08:20:03 +0100

    Remove dead interceptors

--- src/components/UI/WithErrorHandler/WithErrorHandler.js
+++ src/components/UI/WithErrorHandler/WithErrorHandler.js
@@ -8,14 +8,22 @@ const WithErrorHandler = (WrappedComponent, axios) => {
     };
 
     componentWillMount() {
-        axios.interceptors.request.use(request => {
-            this.setState({error:null})
-            return request;
-        })
-
-      axios.interceptors.response.use(response => response, error => {
-        this.setState({ error: error });
+      this.requestInterceptor = axios.interceptors.request.use(request => {
+        this.setState({ error: null });
+        return request;
       });
+
+      this.responseInterceptor = axios.interceptors.response.use(
+        response => response,
+        error => {
+          this.setState({ error: error });
+        }
+      );
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.requestInterceptor);
+      axios.interceptors.request.eject(this.responseInterceptor);
     }
 
     errorConfirmedHandler = () => {
@@ -25,7 +33,10 @@ const WithErrorHandler = (WrappedComponent, axios) => {
     render() {
       return (
         <>
-          <Modal show={this.state.error} modalClosed={this.errorConfirmedHandler}>
+          <Modal
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}
+          >
             {this.state.error ? this.state.error.message : null}
           </Modal>
           <WrappedComponent {...this.props} />

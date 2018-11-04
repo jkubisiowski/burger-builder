aa9c0589d1feef103e47b9e39094c86b95b1b8e9
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2018-11-04 10:34:10 +0100

    Restructure project to use advanced redux

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,7 +8,7 @@ import axios from "../../axios-orders";
 import Spinner from "../../components/UI/Spinner/Spinner";
 import WithErrorHandler from "./../../components/UI/WithErrorHandler/WithErrorHandler";
 import { connect } from "react-redux";
-import * as actionTypes from "../../store/actions";
+import * as actionTypes from "../../store/actions/actionTypes";
 
 class BurgerBuilder extends Component {
   state = {
--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import {createStore} from "redux";
 import { Provider } from "react-redux";
-import reducer from "./store/reducer";
+import reducer from "./store/reducers/burgerBuilder";
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>

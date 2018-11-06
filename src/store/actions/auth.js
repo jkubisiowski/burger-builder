35ee613be0a90a12e1151e534cb5fa8924f763ab
Jakub Kubisiowski <jakub.kubisiowski@fortedigital.no>
2018-11-06 19:29:28 +0100

    Remove console.log()

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -80,7 +80,6 @@ export const auth = (email, password, isSignUp) => {
         returnSecureToken: true
       })
       .then(response => {
-        console.log(response.data);
         const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
         localStorage.setItem("token", response.data.idToken);
         localStorage.setItem("expirationDate", expirationDate);
@@ -89,7 +88,6 @@ export const auth = (email, password, isSignUp) => {
         dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch(error => {
-        console.log(error);
         dispatch(authFail(error.response.data.error));
       });
   };
